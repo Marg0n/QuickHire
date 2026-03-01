3e3b485864a48242919e9bc6ccde906b64467bd7
Margon  <monondcosta@gmail.com>
2026-03-02 03:08:58 +0600

    Add: Added common layout page.

---
+++ src/app/(CommonLayout)/page.tsx
@@ -0,0 +1,16 @@
+
+
+export const metadata = {
+    title: "Quick Hire",
+    description: "Find your jobs here",
+}
+
+const LandingPage = () => {
+    return (
+        <div className="">
+            page here
+        </div>
+    );
+};
+
+export default LandingPage;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
         <Image
          src={serverError} 
          alt="illustration"
-         className="w-full lg:w-[500px]"
+         className="w-full lg:w-125"
          width={500}
          height={500}
         />
