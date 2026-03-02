7a23295132cfa48eddab1c76e90dc25469cddb84
Margon  <monondcosta@gmail.com>
2026-03-02 17:30:30 +0600

    Add: Added loding component.

--- src/components/landing/Body.tsx
+++ src/components/landing/Body.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Body = () => {
     return (
-        <div className='min-h-screen'>
+        <div className='min-h-[80vh]'>
             body here
         </div>
     );
---
+++ src/components/shared/Loading.tsx
@@ -0,0 +1,13 @@
+
+
+const Loading = () => {
+  return (
+    <>
+      <div className="w-7 h-7 animate-[ping_2s_linear_infinite] rounded-full border-2 border-[#4640DE] flex items-center justify-center">
+        <div className="w-5 h-5 animate-[ping_2s_linear_3s_infinite] rounded-full border-2 border-[#4640DE]"></div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
