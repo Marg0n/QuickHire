db4511b9de318d5e8f54113d590ed678ae22726e
Margon  <monondcosta@gmail.com>
2026-03-04 04:45:13 +0600

    Fix: Fixed CTA responsiveness for lg and xl.

--- src/components/landing/newsletter/CTA.tsx
+++ src/components/landing/newsletter/CTA.tsx
@@ -6,28 +6,26 @@ import Image from "next/image";
 const CTA = () => {
   return (
     <div
-      className="xl:py-24 py-14 lg:px-22 px-6 xl:px-60 bg-transparent w-full min-h-[50vh] xl:min-h-94 relative"
-      style={{
-        backgroundImage: `url(${rect.src})`,
-        backgroundPosition: "center",
-        backgroundSize: "auto",
-        backgroundRepeat: "no-repeat",
-        backgroundAttachment: "scroll",
-      }}
+      className="relative"
     >
-      {/* texts */}
-      <div className="flex  flex-col justify-center items-star space-y-4 w-80">
-        <h2 className="text-5xl text-wrap text-white font-bold">Start posting jobs today</h2>
-        <p className="text-white">Start posting jobs for only $10.</p>
-        <Button variant={2} className="bg-white font-semibold w-60">
-          Sign Up For Free
-        </Button>
-      </div>
+      {/* Background  */}
+     <div className="xl:py-24 py-14 lg:px-22 px-0 xl:px-20 bg-transparent  min-h-[50vh] xl:min-h-94  clip-cta lg:mx-[90px] xl:mx-[120px]">
 
-      {/* dashboard pic */}
-      <div className="absolute right-16 xl:right-56 bottom-0">
-        <Image src={dashboard} alt="background" width={500} height={550}/>
-      </div>
+        {/* texts */}
+        <div className="flex  flex-col justify-center items-start space-y-4 w-80 xl:w-94 xl:pl-8 pt-8 ">
+          <h2 className="text-5xl text-wrap text-white font-bold">Start posting jobs today</h2>
+          <p className="text-white">Start posting jobs for only $10.</p>
+          <Button variant={2} className="bg-white font-semibold w-60">
+            Sign Up For Free
+          </Button>
+        </div>
+
+     </div>
+        {/* dashboard pic */}
+        <div className="absolute right-36 xl:right-46 bottom-0 z-10 w-[350px] xl:w-[500px]">
+          <Image src={dashboard} alt="background" height={550}/>
+        </div>
+
     </div>
   );
 };
