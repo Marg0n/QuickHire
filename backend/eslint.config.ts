a6cbe15b65f4eff7223e409ded8982a8639b5581
Margon  <monondcosta@gmail.com>
2026-03-07 05:16:05 +0600

    Add/Modify: Added eslint to the backend and modified tsconfig file according to that.

---
+++ backend/eslint.config.ts
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "no-console": "off",
+      "prefer-const": "warn",
+      "no-undef": "error",
+    },
+  },
+  ...tseslint.configs.recommended,
+]);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import type { Application, Request, Response } from "express";
 import express from "express";
+import cors from "cors";
 
 const app: Application = express();
 
 //* Parsers
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is running!");
