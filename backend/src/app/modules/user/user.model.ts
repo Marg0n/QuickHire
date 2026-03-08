7fabc869b4b7366f3795c194b1a024a78a0318ec
Margon  <monondcosta@gmail.com>
2026-03-08 06:08:00 +0600

    Add: Added jwt, bcrypt and user model.

---
+++ backend/src/app/modules/user/user.model.ts
@@ -0,0 +1,59 @@
+import { model, Schema } from "mongoose";
+import type { IUser } from "./user.interface.js";
+import bcrypt from 'bcrypt';
+import config from "../../config/index.js";
+
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Please provide a name.'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'Please provide email address.'],
+        unique: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: [true, 'Please provide a password.']
+    },
+    role: {
+        type: String,
+        enum: {
+            values: ["admin", "user"],
+            message: '{VALUE} is not valid. Please provide a valid type.',
+        },
+        required: [true, 'Please provide a role.'],
+        default: "user"
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+// Middleware to update 'updatedAt' field before updating a document 
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  //* hashing password and save into DB
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  next();
+});
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
