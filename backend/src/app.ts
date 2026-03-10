854211aa299c63991ae9f9d8e6c9abc0b1fdb0a4
Margon  <monondcosta@gmail.com>
2026-03-10 11:08:47 +0600

    Add: Added UserRoutes to app in backend.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
 import type { Application, Request, Response } from "express";
 import express from "express";
 import cors from "cors";
+import { UserRoutes } from "./app/modules/user/user.route.js";
 
 const app: Application = express();
 
 //* Parsers
 app.use(express.json());
 app.use(cors());
+
+//* Routes
+app.use("api/v1/users", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is running!");
