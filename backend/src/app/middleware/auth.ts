2eb483345bdc69589c700a1f1ee965f236af5556
Margon  <monondcosta@gmail.com>
2026-03-11 15:21:39 +0600

    Add/Modify: Added backend user constant & interface by adding UserModel. Modified backend auth middleware.

---
+++ backend/src/app/middleware/auth.ts
@@ -0,0 +1,48 @@
+import type { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import catchAsync from '../utils/catchAsync.js';
+import AppError from '../errors/AppError.js';
+import config from '../config/index.js';
+import type { TUserRole } from '../modules/user/user.interface.js';
+
+const auth = (...requiredRoles: TUserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    // const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1];
+    const secret = config.jwt_secret;
+
+    //? token check
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    //? validity check
+    let decoded : any; 
+    try {
+      decoded = jwt.verify(
+        token,
+        config.jwt_secret as string,
+      ) as JwtPayload;
+    } catch (err) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'UNAUTHORIZED');
+    }
+
+    const { role } = decoded;
+
+    //? if error
+    if (requiredRoles.length && !requiredRoles.includes(role)) {
+      throw new AppError(
+        httpStatus.FORBIDDEN,
+        'You have no permission to access this route'
+      );
+    }
+
+    //? user decoded
+    req.user = decoded;
+    next();
+  });
+};
+
+export default auth;
--- backend/src/app/modules/user/user.interface.ts
+++ backend/src/app/modules/user/user.interface.ts
@@ -1,4 +1,5 @@
-import { Types } from "mongoose";
+import { Model, Types } from "mongoose";
+import type { USER_ROLE } from "./user.constant.js";
 
 export interface IUser{
     _id?: Types.ObjectId;
@@ -10,3 +11,17 @@ export interface IUser{
     createdAt?: Date;
     updatedAt?: Date;
 }
+
+export interface UserModel extends Model<IUser> {
+  isUserExistsByCustomId(id: string): Promise<IUser>;
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean;
+}
+
+export type TUserRole = keyof typeof USER_ROLE;
