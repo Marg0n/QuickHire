c8ac2729c53cc2bcfca68afa832a68f10f638857
Margon  <monondcosta@gmail.com>
2026-03-11 10:59:25 +0600

    Modify: Modified backend auth route.

--- backend/src/app/modules/auth/auth.route.ts
+++ backend/src/app/modules/auth/auth.route.ts
@@ -1,6 +1,31 @@
 import express from 'express';
+import validateRequest from '../../middleware/validateRequest.js';
+import { userValidations } from '../user/user.validation.js';
+import { authController } from './auth.controller.js';
+import { authValidation } from './auth.validation.js';
 
 const router = express.Router()
 
+router.post(
+  '/register',
+  validateRequest(userValidations.userValidationSchema),
+  authController.register,
+);
+router.post(
+  '/login',
+  validateRequest(authValidation.loginValidationSchema),
+  authController.login,
+);
+router.post(
+  '/refreshToken',
+  validateRequest(authValidation.refreshTokenValidationSchema),
+  authController.refreshToken,
+);
+
+router.put(
+  '/change-password',
+  validateRequest(authValidation.changePasswordValidationSchema),
+  authController.changePassword,
+);
 
 export const AuthRoutes = router;
