a35ca22072e4e8914fdc06b12a18189a0b25027c
Margon  <monondcosta@gmail.com>
2026-03-11 15:33:47 +0600

    Add: Added backend application controller.

---
+++ backend/src/app/modules/application/application.controller.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from "express";
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync.js";
+import { applicationServices } from "./application.service.js";
+import { sendResponse } from "../../utils/sendResponse.js";
+
+const createApplication = catchAsync(async (req: Request, res: Response) => {
+  const result = await applicationServices.createApplicationIntoDB(req.body);
+
+  sendResponse.sendUpdateResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Application submitted successfully!",
+    data: result,
+  });
+});
+
+export const ApplicationController = {
+  createApplication,
+};
