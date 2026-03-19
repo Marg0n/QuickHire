e822c9f04bff571ac4b13671c68db413b4a6f94c
Margon  <monondcosta@gmail.com>
2026-03-19 15:22:33 +0600

    Add: Added dashboard layout and page in frontend.

---
+++ frontend/src/app/(DashboardLayout)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardLandingPage = () => {
+    return (
+        <div>
+            dashboard
+        </div>
+    );
+};
+
+export default DashboardLandingPage;
